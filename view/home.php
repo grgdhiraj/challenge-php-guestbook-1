bb315e3f81d9331da8fe022e86b8d3e0b7e649b6
rafahalani <alani.rafah@yahoo.com>
2019-10-22 14:12:53 +0200

    added a form

--- view/home.php
+++ view/home.php
@@ -11,6 +11,22 @@ declare(strict_types=1);
     <title>GUESTBOOK</title>
 </head>
 <body class="text-center">
+
+    <form id="gb-sign" onsubmit="return gb.save();">
+        <h1>Welcome to our GuestBook</h1>
+        <label for="gb-title">Title:</label>
+        <input id="gb-title" type="text" maxlength="255" required/></br>
+        <label for="gb-date">Date:</label>
+        <input id="gb-date" type="date" maxlength="255" required/></br>
+        <label for="gb-content">Content:</label>
+        <input id="gb-content" type="text" maxlength="255" required/></br>
+        <label for="gb-name"> Name:</label>
+        <input id="gb-name" type="text" maxlength="255" required/></br>
+        <input type="submit" value="Submit"/>
+    </form>
+</body>
+</html>
+</form>
 <form method="get">
 <h3>Guestbook </h3>
     <?php foreach ($allPosts AS $post): ?>
